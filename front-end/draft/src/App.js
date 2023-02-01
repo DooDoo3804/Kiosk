d1c8e31fc641f4c11c29e5e56d535d477e395ff5
k3020044 <k3020044@gmail.com>
2023-02-01 17:25:09 +0900

    Add payment modal

--- front-end/draft/src/App.js
+++ front-end/draft/src/App.js
@@ -1,37 +1,51 @@
 import './App.css';
 import Header from './layout/Header'
 import React, { useState } from 'react';
+// import { useNavigate, Route, Routes } from "react-router-dom"
 import Modal from "./components/Modal";
+// import ModalItem from "./components/ModalItem";
 import EasyImage from "./components/EasyImage";
+// import Payment from "./pages/Payment"
 
 
 function App() {
   // 매장식사, 포장 여부 모달
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [takeoutModal, setTakeoutModal] = useState(false);
 
   // 커스텀 모달
-  const [modalOpen, setModalOpen] = useState(false);
+  const [customModal, setCustomModal] = useState(false);
+
+  // 결제 완료 모달
+  const [payModal, setPayModal] = useState(false);
 
   return (
   <div>
     <Header/>
     <EasyImage/>
-    <div>
+    
           {/* 매장식사, 포장 여부 모달 */}
-      <input type="button" value="결제" onClick={() => setModalIsOpen(!modalIsOpen)}/>
-      {modalIsOpen && (
-        <Modal closeModal={() => setModalIsOpen(!modalIsOpen)}>
+      <input type="button" value="결제" onClick={() => setTakeoutModal(!takeoutModal)}/>
+      {takeoutModal && (
+        <Modal closeModal={() => setTakeoutModal(!takeoutModal)}>
           <div id="rectangle">
-            <div className="box green"> 먹고 가기 </div> 
-            <div className="box green"> 가져 가기 </div>
+            <div className="box green" onClick={() => { 
+              setTakeoutModal(!takeoutModal) 
+              setCustomModal(!customModal)}} > 먹고 가기 </div> 
+            <div className="box green" onClick={() => { 
+              setTakeoutModal(!takeoutModal)
+              setCustomModal(!customModal)}} > 가져 가기 </div>
           </div>
         </Modal>
       )}
-
-        {/* 커스텀 모달 */}
-        <input type="button" value="커스텀" onClick={() => setModalOpen(!modalOpen)}/>
-        {modalOpen && (
-        <Modal closeModal={() => setModalOpen(!modalOpen)}>
+      
+      {/* <Routes>
+        <Route path="/payment" element={<Payment />} />
+      </Routes> */}
+ 
+        {/* 커스텀 모달  */}
+        {/* <input type="button" value="커스텀" onClick={() => setModalOpen(!modalOpen)}/> */}
+        {customModal && (
+        <Modal closeModal={() => setCustomModal(!customModal)}>
           {/* <div class="toggle-button-cover"> */}
           {/* 단품/세트 선택 토글 */}
             <div class="button-cover">
@@ -41,13 +55,25 @@ function App() {
                 <div class="layer"></div>
               </div>
             </div>
+            <button onClick={() => {
+              setCustomModal(!customModal) 
+              setPayModal(!payModal)}}>커스텀 완료</button>
           {/* </div> */}
-          <button>담기</button>
         </Modal>
-        )}    
-    </div>
+        )}
+
+        {/* 결제 완료 모달 */}
+        {payModal && (
+        <Modal closeModal={() => setPayModal(!payModal)}>
+          <div id="rectangle">
+            <div className="box green" onClick={() => setPayModal(!payModal)}>현금</div>
+            <div className="box green" onClick={() => setPayModal(!payModal)}>카카오페이</div>
+          </div>
+        </Modal>
+        )}
   </div>
   );
 }
+
 
 export default App;
--- front-end/draft/src/pages/Payment.js
+++ front-end/draft/src/pages/Payment.js
@@ -1,1 +1,10 @@
 // 결제 완료 메세지 화면
+import React from 'react'
+
+export default function Payment() {
+  return (
+    <div>
+      <h1>주문이 완료되었습니다</h1>
+    </div>
+  )
+}
