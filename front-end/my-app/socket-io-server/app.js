7161294c374940441a2fc763ab3d7925b38e7dd3
kimyujin <heyguys123@naver.com>
2023-02-08 11:12:22 +0900

    update

--- front-end/my-app/socket-io-server/app.js
+++ front-end/my-app/socket-io-server/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = sockeIo(server, {
     cors: {
-        origin: "http://70.12.246.141",
+        origin: ['http://localhost:3000', 'http://70.12.246.87:3000'],
         megthods: ["GET", "POST"]
     }
 });
@@ -23,13 +23,23 @@ let interval;
 
 io.on("connection", (socket) =>{
     console.log("New client connected");
-    if(interval){
-        clearInterval(interval);
-    }
+
+    socket.on('login', function(data){
+        socket.name = data.name;
+        socket.userid = data.userid;
+    });
 
     socket.on("client_msg", (data) =>{
         console.log(`클라이언트에서 보낸 메시지 수신: ${data}`);
-    });
+        socket.broadcast.emit('event_name', data);
+        
+    }); 
+
+    socket.on("client_msg2", (data) =>{
+        console.log(`클라이언트에서 보낸 메시지 수신: ${data}`);
+        
+        socket.broadcast.emit('event_name', data);
+    }); 
 
     socket.on("disconnect", () => {
         console.log("Client disconnected");
--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -7,6 +7,57 @@ import React from 'react';
 import io from 'socket.io-client';
 import { Link, Route, BrowserRouter , Routes } from "react-router-dom";
 import Choose from './layout/Choose';
+import axios from 'axios'
+
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+
+
+const socket = io("http://70.12.246.87:4001");
+socket.connect();
+socket.on('event_name', (data) => {
+  console.log(data)
+})
+
+// axios.get('http://70.12.246.124:8081/inburger/user', {
+// 	headers: {
+//     'Access-Control-Allow-origin' : 'http://70.12.246.124:8081',
+//     'Access-Control-Allow-Credentials': true
+// 	},
+// 	proxy: {
+// 	  host: '70.12.246.87',
+// 	  port: 3000
+// 	}
+// 	}).then(function (response) {
+// 		console.log('response is : ' + response.data[0]["name"]);
+// 	}).catch(function (error) {
+// 		if (error.response) {
+// 		  console.log(error.response.headers);
+// 		} 
+// 		else if (error.request) {
+// 	      console.log(error.request);
+// 		} 
+// 		else {
+// 		  console.log(error.message);
+// 		}
+// 	console.log(error.config);
+// });
+
+
+axios.post('http://70.12.246.124:8081/inburger/user', {
+  name: '1',
+  isEasy: 1,
+  height: 100,
+},{	
+  headers: {
+      'Access-Control-Allow-origin' : 'http://70.12.246.124:8081',
+      'Access-Control-Allow-Credentials': true
+  	},
+  	proxy: {
+  	  host: '70.12.246.87',
+  	  port: 3000
+  	}}).then((response) => {console.log(response.data);})
+.catch((response)=> console.log('errot!'));
+
 
 function App() {
 
