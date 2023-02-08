d22c48c9f06e3f88e1508d68da7300aeb50fbe92
김유진 <heyguys123@naver.com>
2023-02-08 11:23:26 +0900

    Update app.js

--- front-end/socket-io-server/app.js
+++ front-end/socket-io-server/app.js
@@ -14,11 +14,7 @@ const server = http.createServer(app);
 
 const io = sockeIo(server, {
     cors: {
-<<<<<<< HEAD:front-end/my-app/socket-io-server/app.js
         origin: ['http://localhost:3000', 'http://70.12.246.87:3000'],
-=======
-        origin: "http://localhost:3000",
->>>>>>> cf16215b7a86ea90229e90243ac78c8a5e628216:front-end/socket-io-server/app.js
         megthods: ["GET", "POST"]
     }
 });
@@ -26,21 +22,10 @@ const io = sockeIo(server, {
 let interval;
 
 io.on("connection", (socket) =>{
-<<<<<<< HEAD:front-end/my-app/socket-io-server/app.js
     console.log("New client connected");
 
-    socket.on('login', function(data){
-        socket.name = data.name;
-        socket.userid = data.userid;
-    });
-=======
-    console.log("들어옴");
-    if(interval){
-        clearInterval(interval);
-    }
->>>>>>> cf16215b7a86ea90229e90243ac78c8a5e628216:front-end/socket-io-server/app.js
 
-    socket.on("incompany", (data) =>{
+    socket.on("client_msg1", (data) =>{
         console.log(`클라이언트에서 보낸 메시지 수신: ${data}`);
         socket.broadcast.emit('event_name', data);
         
@@ -48,7 +33,6 @@ io.on("connection", (socket) =>{
 
     socket.on("client_msg2", (data) =>{
         console.log(`클라이언트에서 보낸 메시지 수신: ${data}`);
-        
         socket.broadcast.emit('event_name', data);
     }); 
 
