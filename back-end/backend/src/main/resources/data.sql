0e97327c5ec3cb8cf17e1df8e98264e902654315
unknown <dodo327@naver.com>
2023-02-13 13:30:35 +0900

    Docs edit data

--- back-end/backend/src/main/resources/data.sql
+++ back-end/backend/src/main/resources/data.sql
@@ -5,26 +5,52 @@ INSERT INTO table_category(category_name) VALUES("sidemenu");
 INSERT INTO table_category(category_name) VALUES("vegetable");
 INSERT INTO table_category(category_name) VALUES("pattie");
 INSERT INTO table_category(category_name) VALUES("bread");
+INSERT INTO table_category(category_name) VALUES("etc");
+
 
 -- ingredient 데이터 추가
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("bread1", 500, 6) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("bread2", 300, 6) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("bread3", 400, 6) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("pattie1", 500, 5) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("pattie2", 700, 5) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("pattie3", 600, 5) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("vegetable1", 200, 4) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("vegetable2", 300, 4) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("vegetable3", 250, 4) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("beverage1", 400, 3) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("beverage2", 500, 3) ;
-INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("beverage3", 600, 3) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("먹물번_윗면", 500, 6) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("머핀번_윗면", 300, 6) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("기본번_윗면", 400, 6) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("돼지고기", 500, 5) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("소고기", 700, 5) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("계란", 600, 5) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("버섯", 200, 4) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("상추", 300, 4) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("토마토", 250, 4) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("오이", 250, 4) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("양파", 250, 4) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("치즈", 400, 7) ;
+INSERT INTO table_ingredient(ingredient_name, ingredient_price, category_id) VALUES("햄", 500, 7) ;
 
 -- menu 데이터 추가
-INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("불고기 버거", 3500, 1) ;
-INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("더블불고기 버거", 3500, 1) ;
-INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("음료 단품", 1500, 3) ;
-INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("감자튀김 단품", 2000, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("초코 쉐이크", 4000, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("우유", 2000, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("오렌지 주스", 3500, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("스프라이트", 2000, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("환타", 2000, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("딸기쉐이크", 3500, 2) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("코카콜라", 2000, 2) ;
+
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("해쉬브라운", 1500, 3) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("프렌치 프라이", 1500, 3) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("바닐라 선데이 아이스크림", 1500, 3) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("상하이치킨스낵랩", 2500, 3) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("초코 선데이 아이스크림", 1500, 3) ;
+
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("빅맥", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("치즈버거", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("불고기버거", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("더블치즈", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("쿼터파운더 치즈", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("베이컨 토마토 디럭스", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("에그 불고기버거", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("불고기버거 세트", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("치즈버거 세트", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("더블치즈버거 세트", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("쿼터파운더 치즈버거 세트", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("베이컨 토마토 디럭스 세트", 3500, 1) ;
+INSERT INTO table_menu(menu_name, menu_price, category_id) VALUES("에크 불고기버거 세트", 3500, 1) ;
 
 -- 저장 과정
 -- user 데이터 추가
