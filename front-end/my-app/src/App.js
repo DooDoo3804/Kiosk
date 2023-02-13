38cbcc97b7d9266abede28f70c9441b000cfc5fe
unknown <dodo327@naver.com>
2023-02-13 16:56:38 +0900

    Docs edit app.js

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -12,13 +12,13 @@ import NomalMode from './layout/NormalMode';
 
 function Callaxios({name, type, Height}){
 
-axios.post('http://70.12.246.124:8081/inburger/menu/user', {
+axios.post('http://3.36.49.220:8081/inburger/menu/user', {
   name: {name},
   isEasy: {type},
   height: {Height},
 },{	
   headers: {
-      'Access-Control-Allow-origin' : 'http://70.12.246.124:8081',
+      'Access-Control-Allow-origin' : 'http://3.36.49.220:8081',
       'Access-Control-Allow-Credentials': true
   	},
   	proxy: {
@@ -33,15 +33,20 @@ function App() {
   const [type, settype] = useState(0);
   const [Height, setHeightt] = useState();
   const [username, setusername] = useState();
+  const [pullData, setdata] = useState('');
   
   const socket = io("http://3.36.49.220:4001");
   socket.connect();
   socket.on('react', (data) => {
-    console.log(data)
-    settype(data["mode"]);
-    setHeightt(data["height"]);
-    setusername(data["name"]);
+
+    setdata = data;
+
+    console.log(pullData)
+    settype(pullData["mode"]);
+    setHeightt(pullData["height"]);
+    setusername(pullData["name"]);
     Callaxios({type, username, Height});
+    setdata = '';
   });
 
 
