4de1f77439f7437e1c3f5d04e26a4c8ab54ab459
unknown <dodo327@naver.com>
2023-02-14 11:00:26 +0900

    Docs

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -56,6 +56,9 @@ function App() {
       settype(data['mode']);
       setHeightt(data['height']);
       setusername(data['name']);
+
+      typeof(data['name'], data['mode'], data['height']);
+
       setcall(!callfunction);
     
     });
